3e2f18fcdf5c31697f6ede451da18aeba44b00e7
Rekkusu <ahm_brouwers@hotmail.com>
2019-04-14 14:24:28 +0200

    Add balloon and relative code to the scene

---
+++ Assets/VRChatWorlds/Scripts/FollowTarget.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour {
+
+    [SerializeField]
+    private Transform _target;
+
+    [SerializeField]
+    private Vector3 _offset;
+
+    [SerializeField]
+    private float _followSpeed = 10;
+
+    private Transform _transform;
+
+	private void Start()
+    {
+        _transform = transform;
+    }
+
+    private void Update()
+    {
+        Follow();   
+    }
+
+    private void Follow()
+    {
+        _transform.position = Vector3.Lerp(_transform.position, _target.position + _offset, Time.deltaTime * _followSpeed);
+    }
+}
---
+++ Assets/VRChatWorlds/Scripts/NegativeGravity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class NegativeGravity : MonoBehaviour {
+
+    [SerializeField]
+    private float _forceMultiplier = 1.0f;
+
+    private Rigidbody _rigidbody;
+
+	private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+	}
+
+	private void FixedUpdate()
+    {
+        _rigidbody.AddForce(Vector3.up * Time.deltaTime * _forceMultiplier, ForceMode.Force);
+	}
+}
