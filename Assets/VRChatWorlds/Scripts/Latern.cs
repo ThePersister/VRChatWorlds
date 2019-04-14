5a7924b22d53430c03e56d7bf2d3a2f6f6666ada
Rekkusu <ahm_brouwers@hotmail.com>
2019-04-14 18:30:24 +0200

    Improve lanterns, spread some outside and inside, improve lighting in scene

---
+++ Assets/VRChatWorlds/Scripts/Latern.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latern : MonoBehaviour
+{
+    [SerializeField]
+    private List<MeshRenderer> _laternMeshes;
+
+    [SerializeField]
+    private Light _laternLight;
+
+    [SerializeField]
+    private float _minColorElementValue = 0.5f;
+
+    private void Start()
+    {
+        Color randomColor = new Color(GetRandomColorElement(), GetRandomColorElement(), GetRandomColorElement());
+        foreach (MeshRenderer meshRenderer in _laternMeshes)
+        {
+            meshRenderer.material.color = randomColor;
+        }
+
+        _laternLight.color = randomColor;
+    }
+
+    private float GetRandomColorElement()
+    {
+        return Mathf.Max(_minColorElementValue, Random.value);
+    }
+}
