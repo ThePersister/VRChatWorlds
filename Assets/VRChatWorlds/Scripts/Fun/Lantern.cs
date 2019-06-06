15092671b2974910e087d66e7f4a4051ffacd420
Rekkusu <ahm_brouwers@hotmail.com>
2019-06-06 21:17:31 +0200

    Move scripts to more appropriate folders

--- Assets/VRChatWorlds/Scripts/Fun/Lantern.cs
+++ Assets/VRChatWorlds/Scripts/Fun/Lantern.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Latern : MonoBehaviour
+public class Lantern : MonoBehaviour
 {
     [SerializeField]
     private List<MeshRenderer> _laternMeshes;
